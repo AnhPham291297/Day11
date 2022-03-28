00479cd718052b569d232915773553c0886f4cf1
Pham_Anh_Dev <91934986+AnhPham291297@users.noreply.github.com>
2022-03-28 17:37:55 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
-const modal = document.querySelector(".modal");
-const btn1 = document.querySelector(".btn1");
-const btn2 = document.querySelector(".btn2");
-const btn3 = document.querySelector(".btn3");
-const btn4 = document.querySelector(".btn4");
-const btn5 = document.querySelector(".btn5");
-const myH1 = document.querySelector("#myH1");
+const buttons = document.querySelectorAll(".btn");
+const item = document.querySelector(".item");
 
-btn1.onclick = () => {
-  modal.classList.toggle("hidden");
-};
+const types = [
+  {
+    class: "circle-className",
+    typeName: "circle",
+  },
+  {
+    class: "disappear-className",
+    typeName: "disappear",
+  },
+  {
+    class: "changeColor-className",
+    typeName: "change-color",
+  },
+  {
+    class: "bigger-className",
+    typeName: "bigger",
+  },
+];
 
-btn2.onclick = () => {
-  modal.classList.toggle("change_color");
-};
-
-btn3.addEventListener("click", () => {
-  modal.classList.toggle("open");
+buttons.forEach((btn) => {
+  btn.onclick = () => {
+    const typeBtn = btn.getAttribute(["typeButton"]);
+    const chooseType = types.find((t) => t.typeName === typeBtn);
+    types.forEach((t) => {
+      item.classList.remove(t.class);
+    });
+    item.classList.add(chooseType.class);
+  };
 });
-
-btn4.onclick = () => {
-  myH1.innerHTML = "Tenology school";
-  myH1.style.color = "red";
-};
-
-btn5.onclick = () => {
-  modal.classList.toggle("hidden");
-  myH1.innerHTML = "Mindx";
-  myH1.style.color = "#000";
-  modal.classList.remove("change_color");
-};
